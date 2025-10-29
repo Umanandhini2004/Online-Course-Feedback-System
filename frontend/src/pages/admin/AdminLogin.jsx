2d13a08d449c86eb1ab0060ba4b763e5938b2da7
Umanandhini2004 <bumanandhini04@gmail.com>
2025-10-29 23:50:01 +0530

    Update AdminLogin.jsx

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -19,7 +19,8 @@ const AdminLogin = () => {
     setIsLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/admin/login", form);
+      const res = await axios.post("https://online-course-feedback-system-yf47.vercel.app/api/admin/login", form);
+
       Swal.fire({
         icon: "success",
         title: "Welcome Admin!",
